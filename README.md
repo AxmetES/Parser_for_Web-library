cc494a9ad08b3d0719994557eec1a1eb4c46b2df
axmetES <fraktsia@gmail.com>
2020-06-23 13:18:13 +0600

    - Created Readme.md file.

---
+++ README.md
@@ -0,0 +1,47 @@
+#### Script for pars book site.
+
+ Script for pars [book site tululu.org](http://tululu.org/l55)
+
+#### Getting Started
+ To pars all pages:
+```commandline
+python parse_tululu_category.py
+```
+
+ Script can take special commands (optional):
+```commandline
+--start_page
+``` 
+ Indicates page since which parse started. 
+ Default pars started from first page.
+ 
+ ```commandline
+--end_page
+``` 
+ Indicates page which parse will be end. Default pars ended by lats page.
+
+```commandline
+--dist_folder
+```
+ Indicates folder for parsed files.By default, the files is created in the dist_folder.
+
+```commandline
+--skip_imgs
+```
+ Flag, if exist all images wouldn't download.
+ 
+ ```commandline
+--skip_txt
+``` 
+ Flag, if exist all txt wouldn't download.
+  
+ ```commandline
+--json_path
+```
+ Indicates folder for json file which would created by the script.
+ By default, the file is created in the dist_folder.
+ 
+ #### Motivation
+ 
+ The code is written for educational purposes - this is a lesson in Python and web development on the site [Devman] (https://dvmn.org).
+  
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -49,19 +49,25 @@ def get_soup(url):
 
 
 def get_link(page_pars):
-    link = None
     links = page_pars.select('table.d_book a')
     for a in links:
-        if 'txt' in a['href']:
-            link = a['href']
-    return link
+        href = a.get('href')
+        if 'txt.php' in href:
+            return href
 
 
 def get_argpars(lust_page):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start_page', type=int, help='input start page number to pars.')
+    parser.add_argument('--start_page', type=int, nargs='?', default=1, help='input start page number to pars.')
     parser.add_argument('--end_page', type=int, nargs='?', default=lust_page,
                         help='input end page number to pars.')
+    parser.add_argument('--dist_folder', type=str, nargs='?', default='dest_folder')
+    parser.add_argument('--skip_imgs', action='store_false', default=True,
+                        help='''input '--skip_imgs' if want skip downloading image files.''')
+    parser.add_argument('--skip_txt', action='store_false', default=True,
+                        help='''input '--skip_txt' if want skip download text files.''')
+    parser.add_argument('--json_path', type=str, nargs='?', default=os.path.join('dest_folder', 'json'),
+                        help='input path to json file.')
     return parser
 
 
@@ -69,22 +75,28 @@ def get_end_page(main_page_url):
     soup = get_soup(main_page_url)
     pages = soup.select('.center a')
     page = [page.text for page in pages][-1:]
-    lust_page = int(''.join(page))+1
+    lust_page = int(''.join(page)) + 1
     return lust_page
 
 
 def main():
-    img_directory = 'imgs'
-    os.makedirs(img_directory, exist_ok=True)
-    book_directory = 'books'
-    os.makedirs(book_directory, exist_ok=True)
-    dir_json = 'json'
-    os.makedirs(dir_json, exist_ok=True)
     page_num = 0
     main_page_url = 'http://tululu.org/l55'
     lust_page = get_end_page(main_page_url)
     parser = get_argpars(lust_page)
     args = parser.parse_args()
+
+    main_distr = args.dist_folder
+    os.makedirs(main_distr, exist_ok=True)
+
+    img_directory = os.path.join(main_distr, 'imgs')
+    os.makedirs(img_directory, exist_ok=True)
+
+    book_directory = os.path.join(main_distr, 'books')
+    os.makedirs(book_directory, exist_ok=True)
+
+    dir_json = args.json_path
+    os.makedirs(dir_json, exist_ok=True)
 
     books_catalog = []
     for page in range(args.start_page, args.end_page):
@@ -92,18 +104,22 @@ def main():
         soup = get_soup(url)
         books = soup.select("table.d_book")
         for book in books:
-            page_num += 1
-            book_author = ''.join(book.select_one('a')['title'].split(' - ')[:1])
-            book_title = ''.join(book.select_one('a')['title'].split(' - ')[1:])
+
             img_id = book.select_one('img')['src']
             book_id = book.select_one('a')['href']
             book_url = make_url(url, book_id)
             page_pars = get_soup(book_url)
-            link_txt = get_link(page_pars)
+            download_link = get_link(page_pars)
 
-            if link_txt is not None:
-                book_path = download_txt(book_url, page_num, book_title, book_directory, link_txt)
-                img_path = download_image(url, img_id, img_directory)
+            if download_link is not None:
+                book_path = img_path = "no text or image, or downloading was skipped"
+                page_num += 1
+                book_title = ''.join(book.select_one('a')['title'].split(' - ')[1:])
+                book_author = ''.join(book.select_one('a')['title'].split(' - ')[:1])
+                if args.skip_txt:
+                    book_path = download_txt(book_url, page_num, book_title, book_directory, download_link)
+                if args.skip_imgs:
+                    img_path = download_image(url, img_id, img_directory)
 
                 comments_pars = page_pars.select(".black")
                 comments = [comment.text.replace('\n', '') for comment in comments_pars][10:]
@@ -123,7 +139,7 @@ def main():
                 }, )
 
     with open(os.path.join(dir_json, 'books_catalog.json'), 'w', encoding='utf8') as file:
-        json.dump(books_catalog, file, ensure_ascii=False, sort_keys=True, indent=4)
+        json.dump(books_catalog, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
